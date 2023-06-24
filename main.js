3277a039d3b058a1ed9ceb4d04e6042eb265b4df
laporka <125121347+laporka@users.noreply.github.com>
2023-06-24 10:13:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.querySelector("#toggle-theme");
+toggleBtn.addEventListener("click", function() {
+    if(document.documentElement.hasAttribute("theme")){
+        document.documentElement.removeAttribute("theme");
+    }
+    else{
+        document.documentElement.setAttribute("theme", "dark");
+    }
+}); 
